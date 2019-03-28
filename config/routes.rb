0e77d6dbeb326769d3d868a5b42113a6de9f5aad
ARYPHAMM <quoctienphamm@gmail.com>
2019-03-28 15:24:35 +0700

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/contact'
+  get 'page/index'
+  get 'page/home'
   resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'places/changestatus', to: 'places#changestatus'
-  root 'places#new'
+  root 'pages#home'
 end
