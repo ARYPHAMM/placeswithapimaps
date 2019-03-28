acf9ce196be01cee1c790f1ee10412b88c5e580d
ARYPHAMM <quoctienphamm@gmail.com>
2019-03-28 14:14:14 +0700

    up

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'places/changestatus', to: 'places#changestatus'
-  
+  root 'places#new'
 end
